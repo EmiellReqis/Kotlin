3cec94b7e8b6b62be654d3f6e57f2a82798f6a2d
EmiellReqis <pawel.tkacz93@gmail.com>
2022-05-20 10:32:56 +0200

    add buttons limit

--- CryptoCurrencyNotes/app/src/main/java/com/paweltkacz/cryptocurrencynotes/MainActivity.kt
+++ CryptoCurrencyNotes/app/src/main/java/com/paweltkacz/cryptocurrencynotes/MainActivity.kt
@@ -2,26 +2,35 @@ package com.paweltkacz.cryptocurrencynotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    var i : Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val addNewButton : ImageButton = findViewById(R.id.addNewButton)
         addNewButton.setOnClickListener{
-            Toast.makeText(this,"New Button Added", Toast.LENGTH_LONG).show()
             addNewButtonToScrollMenu()
         }
     }
 
     fun addNewButtonToScrollMenu(){
+        val maxButtonsNumber : Int = 5
         val linearLayout1 = findViewById(R.id.LinearLayout1) as LinearLayout
         val imageButton = ImageButton(this)
-        imageButton.setImageResource(R.drawable.addednewbutton)
-        linearLayout1.addView(imageButton)
+        if(i > maxButtonsNumber){
+            Toast.makeText(this, "You can't add more buttons, please upgrade account to premium", Toast.LENGTH_SHORT).show()
+        }else{
+            imageButton.setImageResource(R.drawable.addednewbutton)
+            linearLayout1.addView(imageButton)
+            Toast.makeText(this,"New Button Added", Toast.LENGTH_SHORT).show()
+            i++
+        }
     }
 }
